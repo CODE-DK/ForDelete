68f8d200861b6a2891f7198d5c8cc7ce10c073ed
DK <dmitry.komovsky@raiffeisen.ru>
2021-09-05 20:30:27 +0500

    This is the second commit

--- src/com/example/dkproject/Solution.java
+++ src/com/example/dkproject/Solution.java
@@ -3,5 +3,6 @@ package com.example.dkproject;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is the second commit");
     }
 }
