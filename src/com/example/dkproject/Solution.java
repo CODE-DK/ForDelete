d46662ea99cb9eec06dcac4350b89ae67c2759a8
DK <dmitry.komovsky@raiffeisen.ru>
2021-09-05 20:48:02 +0500

    123: This is commit from

--- src/com/example/dkproject/Solution.java
+++ src/com/example/dkproject/Solution.java
@@ -5,6 +5,7 @@ public class Solution {
         System.out.println("Hello world!");
         System.out.println("This is the second commit");
 
+        System.out.println("This is some task from feature/123");
         System.out.println("THis is new commit for main branch");
     }
 }
