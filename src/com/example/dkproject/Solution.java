850a59d698ac2072c3936de06aef1de345408e9a
DK <dmitry.komovsky@raiffeisen.ru>
2021-09-05 21:08:44 +0500

    This is some local changes

--- src/com/example/dkproject/Solution.java
+++ src/com/example/dkproject/Solution.java
@@ -7,5 +7,7 @@ public class Solution {
 
         System.out.println("This is some task from feature/123");
         System.out.println("THis is new commit for main branch");
+
+        System.out.println("This is some changes for local state.");
     }
 }
