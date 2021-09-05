57ca6d194e74afd277fa978061769670ae7f3d84
DK <dmitry.komovsky@raiffeisen.ru>
2021-09-05 20:49:56 +0500

    This is additional commit for main

--- src/com/example/dkproject/Solution.java
+++ src/com/example/dkproject/Solution.java
@@ -4,5 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is the second commit");
+
+        System.out.println("THis is new commit for main branch");
     }
 }
